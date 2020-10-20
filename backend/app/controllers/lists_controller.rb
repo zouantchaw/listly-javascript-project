ec2cebe20db036f84cf416b702e1098188185475
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-10-20 19:42:53 -0400

    List controller show

--- backend/app/controllers/lists_controller.rb
+++ backend/app/controllers/lists_controller.rb
@@ -2,7 +2,12 @@ class ListsController < ApplicationController
 
     def index
         @lists = List.all
-        render json: @lists.as_json() 
+        render json: @lists.as_json(include: {list_items: {only:[:content]}}) 
+    end
+
+    def show
+        @list = List.find_by[:id params[:id]]
+        render json: @list.as_json(include: {list_items: {only:[:content]}}) 
     end
 end
  
