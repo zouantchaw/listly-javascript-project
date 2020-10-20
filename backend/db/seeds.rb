911e2991b0b1dfe6c5b89dd4f15acbf29fe4afc7
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-10-19 20:00:37 -0400

    setting seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+list_a = List.create(name: "Shopping List")
+list_b = List.create(name: "Weekly Workout")
+list_c = List.create(name: "Fav Books")
+list_d = List.create(name: "Grades")
