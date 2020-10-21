76a6778719a4132654210b34d89a8e299e27d9d8
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-10-20 21:22:50 -0400

    loadlist function

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,20 @@
+// Selectors 
+const listNav = document.getElementById("all-lists")
+
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("The DOM has Loaded")
+    fetch("http://localhost:3000/lists")
+    .then(response => response.json())
+    .then(json => loadLists(json))
 })
+
+function loadLists(object) {
+    for (const list of object) {
+        let listItemDiv = document.createElement('div')
+        listItemDiv.className = "todo"
+        let listItem
+        let listElement = document.createElement('li')
+        listElement.className = "list-item"
+        listElement.innerText = list.name 
+        listNav.appendChild(listElement)
+    }
+}
