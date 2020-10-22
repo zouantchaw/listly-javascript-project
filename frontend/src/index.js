8a85bbac31aec0f467fc3ec2fa5114d82a07b1c0
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-10-21 23:27:01 -0400

    laoded items onto main screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ const LISTS_URL = `${BASE_URL}/lists`;
 
 // Selectors 
 const listNav = document.querySelector(".all-lists")
+const taskList = document.querySelector(".task-list")
 
 
 // When the DOM Loads:
@@ -22,15 +23,36 @@ function fetchLists() {
 }
 
 function displayListAndItems(json) {
+    document.querySelector('#main-section-header').innerHTML = json[0].name
     for (const list of json) {
-        // let listItemDiv = document.createElement('div')
-        // listItemDiv.className = "todo"
         let listElement = document.createElement('li')
         listElement.className = "list-item"
         listElement.innerText = list.name 
         listNav.appendChild(listElement)
     }
+    for (const listItem of json[0].list_items) {
+        let listItemDiv = document.createElement('div')
+        listItemDiv.className = "todo"
+
+        let listItemLi = document.createElement('li')
+        listItemLi.className = "todo-item"
+        listItemLi.innerText = listItem.content
+
+        let listItemBtn1 = document.createElement('button')
+        listItemBtn1.className = "complete-btn"
+        listItemBtn1.innerHTML = `<i class="fas fa-check"></i>`
+
+        let listItemBtn2 = document.createElement('button')
+        listItemBtn2.className = "trash-btn"
+        listItemBtn2.innerHTML = `<i class="fas fa-trash"></i>`
+
+        listItemDiv.appendChild(listItemLi)
+        listItemDiv.appendChild(listItemBtn1)
+        listItemDiv.appendChild(listItemBtn2)
+        taskList.appendChild(listItemDiv)
+    }        
 };
+
 
 
 
