9681ffdedd8f8c390b70efad48010395b3fea314
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-10-22 11:11:00 -0400

    abstrcated domconted loaded

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -56,23 +56,3 @@ function displayListAndItems(json) {
 
 
 
-
-
-
-// document.addEventListener("DOMContentLoaded",  () {
-//     fetch("http://localhost:3000/lists")
-//     .then(response => response.json())
-//     .then(json => loadLists(json))
-// })
-
-// function loadLists(object) {
-//     for (const list of object) {
-//         let listItemDiv = document.createElement('div')
-//         listItemDiv.className = "todo"
-//         // let listItem
-//         let listElement = document.createElement('li')
-//         listElement.className = "list-item"
-//         listElement.innerText = list.name 
-//         listNav.appendChild(listElement)
-//     }
-// }
